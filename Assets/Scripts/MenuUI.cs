d233db9a1a0b05e753d226ecd9bcbb98ad1dd974
JUSTICE-DE-TORE\PatrX <patrx.rsg@gmail.com>
2022-07-12 09:40:39 +0200

    final commit

--- Assets/Scripts/MenuUI.cs
+++ Assets/Scripts/MenuUI.cs
@@ -1,44 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using UnityEditor;
 
-[DefaultExecutionOrder(1000)]
 public class MenuUI : MonoBehaviour
 {
-    [SerializeField] Text scoreText;
+    [SerializeField] Text highScoreText;
     [SerializeField] InputField playerName;
 
 
     private void Start()
     {
-        if (ScoreKeeper.Instance.maxScorePlayerName != "")
+        if (!string.IsNullOrEmpty(ScoreKeeper.Instance.maxScorePlayerName))
         {
             string name = ScoreKeeper.Instance.maxScorePlayerName;
             int score = ScoreKeeper.Instance.maxScore;
-            scoreText.text = $"Best Score by {name} : {score}";
-        }
-        else
-        {
-            scoreText.text = "";
+            highScoreText.gameObject.SetActive(true);
+            highScoreText.text = $"Best Score by {name} : {score}";
         }
     }
 
     public void StartNew()
     {
-        ScoreKeeper.Instance.newPlayerName = playerName.text;
+        if (!string.IsNullOrEmpty(playerName.text))
+        {
+            ScoreKeeper.Instance.newPlayerName = playerName.text;
+        }
+        else 
+        {
+            ScoreKeeper.Instance.newPlayerName = "Anonymous";
+        }
         SceneManager.LoadScene(1);
     }
 
     public void Exit()
     {
-#if UNITY_EDITOR
-        EditorApplication.ExitPlaymode();
-#else
-        Application.Quit(); // original code to quit Unity player
-#endif
+        Application.Quit();
     }
-
 }
